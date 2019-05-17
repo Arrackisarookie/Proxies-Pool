efd13c539460def23e7a6fcbc79369011c11d479
Arrackisarookie <nicearrack@163.com>
2019-05-17 14:22:01 +0800

    add exceptions

--- handle/tester.py
+++ handle/tester.py
@@ -20,9 +20,14 @@ class Tester(object):
                 proxy = proxy.decode('utf-8')
             try:
                 real_proxy = 'http://' + proxy
-                async with session.get(TEST_URL, proxy=real_proxy, timeout=10, allow_redirects=False) as response:
+                async with session.get(
+                    TEST_URL, proxy=real_proxy,
+                    timeout=10, allow_redirects=False
+                ) as response:
                     if response.status in VALID_STATUS_CODE:
-                        print('Available:', proxy, 'Current score:', self.redis.max(proxy))
+                        print(
+                            'Available:', proxy,
+                            'Current score:', self.redis.max(proxy))
                     else:
                         self.redis.decrease(proxy)
                         print('Status_Code:', response.status, 'IP:', proxy)
@@ -35,7 +40,9 @@ class Tester(object):
                 ClientResponseError,
                 ServerDisconnectedError
             ):
-                print('Failed to request', proxy, 'Current score:', self.redis.decrease(proxy))
+                print(
+                    'Failed to request', proxy,
+                    'Current score:', self.redis.decrease(proxy))
 
     def run(self):
         print('Tester started...')
